c2fec7f67f26d64b5b7a4b32e73e2270f980bf7b
Sahil Saha <sahilsaha7773@gmail.com>
2022-04-15 22:45:55 +0530

    initial styling

---
+++ dockerfile
@@ -0,0 +1,14 @@
+# pull the official base image
+FROM node:alpine
+# set working direction
+WORKDIR /app
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+# install application dependencies
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm i
+# add app
+COPY . ./
+# start app
+CMD ["npm", "start"]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { Badge, Table, Button, Form } from 'react-bootstrap';
+import { PieChart } from 'react-minimal-pie-chart';
 import apiConfig from '../configs/apiConfig';
 import problems from '../data/problems';
+import styles from './styles/home.module.css';
 
 function Home() {
-  const [username, setUsername] = useState("SahilS");
+  const [username, setUsername] = useState("");
+  const [usernameInp, setUsernameInp] = useState("");
   const [topic, setTopic] = useState("all");
   const [userData, setUserData] = useState({});
 
@@ -15,30 +19,55 @@ function Home() {
         console.log(data);
         setUserData(data);
       })
-  }, []);
+  }, [, username]);
   return (
-    <div>Home
-      {
-        Object.keys(userData).map((topic) => (
-          <div>
-            <h1>{topic}</h1>
-            {userData[topic].map(problem => (
-              <div>
-                <a target="_blank" href={problem.url}>{problem.url}</a>
-                {problem.attempted ? (problem.verdict) : ("Not Attempted")}
-              </div>
-            ))}
-          </div>
-        ))
-      }
-      {/* {problems.data.map(topic => (
-        <div>
-          <h1>{topic.topic}</h1>
-          {topic.data.map(problem => (
-            <h3>{problem}</h3>
-          ))}
-        </div>
-      ))} */}
+    <div>
+      <div className={styles.usernameCard}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>CF Username</Form.Label>
+            <Form.Control onChange={(e) => setUsernameInp(e.target.value)} placeholder="Enter CF username" />
+            <Form.Text className="text-muted">
+              We'll never share your CF username with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" onClick={(e) => setUsername(usernameInp)}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <div>
+        {
+          Object.keys(userData).map((topic) => (
+            <div className={styles.topicContainer}>
+              <h2>{topic}</h2>
+              <p>{userData[topic].attempted} Attempted, {userData[topic].solved} Solved, {userData[topic].wa} not Accepted</p>
+              <Table hover striped responsive className={styles.problemsTable}>
+                <thead>
+                  <th>Problem</th>
+                  <th>Submissions</th>
+                </thead>
+                <tbody>
+                  {userData[topic].problems.map(problem => (
+                    // <tr className={`${(problem.attempted && (problem.verdict == "OK" && styles.green))} ${(problem.attempted && (problem.verdict == "WRONG_ANSWER" && styles.red))}`}>
+                    <tr>
+                      <td><a target="_blank" href={problem.url}>{problem.url}</a></td>
+                      <td >{problem.attempted ?
+                        <Badge bg={problem.verdict === "OK" ? "success" : "danger"}>{problem.verdict}</Badge>
+                        :
+                        // <Badge bg="dark">Not attempted</Badge>
+                        "Not attempted"
+                      }</td>
+                    </tr>
+                  ))}
+                </tbody>
+
+              </Table>
+
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
